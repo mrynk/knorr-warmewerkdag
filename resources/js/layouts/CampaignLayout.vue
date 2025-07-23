c0c6f1631942aee4c5a94302bae601573076d610
Michel Ypma <michel@mrynk.nl>
2025-07-24 00:00:26 +0200

    first draft of design

---
+++ resources/js/layouts/CampaignLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import RewardsHeader from '@/components/RewardsHeader.vue';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Link } from '@inertiajs/vue3';
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>();
+</script>
+
+<template>
+    <div class="flex min-h-svh flex-col items-center bg-black">
+        <RewardsHeader />
+        <div class="relative z-10 w-full">
+            <div class="w-full h-[100px] bg-gradient-to-b from-black/0 to-black/80 absolute -top-[100px] left-0"></div>
+            <div class="bg-black/80 flex w-full flex-col gap-6 items-center">
+                <div class="flex flex-col w-full">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/pages/Landing.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import CampaignLayout from '@/layouts/CampaignLayout.vue';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+    PinInput,
+    PinInputGroup,
+    PinInputSlot,
+} from '@/components/ui/pin-input'
+import { ChevronRight } from 'lucide-vue-next';
+
+const value = ref<string[]>([''])
+const handleComplete = (e: string[]) => alert(e.join(''))
+</script>
+
+<template>
+    <CampaignLayout>
+        <div class="bg-gradient-to-b from-black/0 via-black/0 to-[#4E8B45]">
+            <div class="flex flex-col gap-8 items-center px-6 py-10 max-w-xl mx-auto">
+                <img src="/static/title.png" alt="Soep op? Tijd voor je prijs@" class="w-full h-auto" />
+                <p class="text-center text-white text-2xl font-bold p-2">Vul je code in en ontdek direct of je prijs
+                    hebt!
+                </p>
+                <form action="" class="flex flex-col gap-4 w-full items-center">
+                    <PinInput id="pin-input" v-model="value" placeholder="-" @complete="handleComplete">
+                        <PinInputGroup class="w-full">
+                            <PinInputSlot v-for="(id, index) in 8" :key="id" :index="index"
+                                class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                        </PinInputGroup>
+                    </PinInput>
+                    <Input type="text" placeholder="Naam"
+                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                    <Input type="text" placeholder="E-mail"
+                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                    <Input type="text" placeholder="Welke soep?"
+                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                    <p class="text-sm text-white p-2 text-center">
+                        Door deel te nemen met deze actie ga je akkoord met onze <a href="#"
+                            class="text-white underline">actievoorwaarden</a>.
+                    </p>
+                    <Button type="submit" class="w-2/3 bg-[#F8BA00] text-white">Check mijn code
+                        <ChevronRight class="w-4 h-4" />
+                    </Button>
+                </form>
+            </div>
+        </div>
+        <div class="w-full bg-[#4E8B45]">
+            <div class="flex flex-col gap-8 items-center px-6 py-10 max-w-xl mx-auto">
+                <img src="/static/soup-title.png" alt="Soep op? Tijd voor je prijs@" class="w-3/4 h-auto my-8" />
+                <video src="/static/video.mp4" paused controls class="aspect-1/1 object-cover w-full xl:aspect-16/9" />
+            </div>
+        </div>
+    </CampaignLayout>
+</template>
