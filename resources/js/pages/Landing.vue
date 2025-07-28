37d48724d76375eea598388f90552b733173dd77
Michel Ypma <michel@mrynk.nl>
2025-07-29 00:00:27 +0200

    Added select component and soup options

--- resources/js/pages/Landing.vue
+++ resources/js/pages/Landing.vue
@@ -9,6 +9,8 @@ import {
     PinInputSlot,
 } from '@/components/ui/pin-input'
 import { ChevronRight } from 'lucide-vue-next';
+import { Separator } from '@/components/ui/separator';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const value = ref<string[]>([''])
 const handleComplete = (e: string[]) => alert(e.join(''))
@@ -23,18 +25,28 @@ const handleComplete = (e: string[]) => alert(e.join(''))
                     hebt!
                 </p>
                 <form action="" class="flex flex-col gap-4 w-full items-center">
-                    <PinInput id="pin-input" v-model="value" placeholder="-" @complete="handleComplete">
-                        <PinInputGroup class="w-full">
+                    <PinInput id="pin-input" v-model="value" placeholder="-" class="h-16">
+                        <PinInputGroup class="w-full h-16">
                             <PinInputSlot v-for="(id, index) in 8" :key="id" :index="index"
-                                class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                                class="w-full h-16 dark:bg-white dark:text-black dark:placeholder:text-black/80 text-2xl m-1" />
                         </PinInputGroup>
                     </PinInput>
                     <Input type="text" placeholder="Naam"
-                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                        class="w-full text-2xl md:text-xl dark:bg-white dark:text-black dark:placeholder:text-black/80" />
                     <Input type="text" placeholder="E-mail"
-                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
-                    <Input type="text" placeholder="Welke soep?"
-                        class="w-full dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                        class="w-full text-2xl md:text-xl dark:bg-white dark:text-black dark:placeholder:text-black/80" />
+                    <Select>
+                        <SelectTrigger
+                            class="w-full text-2xl md:text-xl dark:bg-white dark:text-black dark:placeholder:text-black/80">
+                            <SelectValue placeholder="Welke soep heb je gegeten?" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="tomatensoep">Tomatensoep</SelectItem>
+                            <SelectItem value="erwtensoep">Erwtensoep</SelectItem>
+                            <SelectItem value="kippensoep">Kippensoep</SelectItem>
+                            <SelectItem value="groentesoep">Groentesoep</SelectItem>
+                        </SelectContent>
+                    </Select>
                     <p class="text-sm text-white p-2 text-center">
                         Door deel te nemen met deze actie ga je akkoord met onze <a href="#"
                             class="text-white underline">actievoorwaarden</a>.
