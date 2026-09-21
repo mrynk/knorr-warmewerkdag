3a3602d4711cea58b8d8b4078ec9a3f26cd15ee9
Michel Ypma <michel@mrynk.nl>
2026-09-21 12:02:43 +0200

    extra codes

--- config/unique_codes.php
+++ config/unique_codes.php
@@ -10,5 +10,25 @@ return [
             'name' => 'Appel',
             'amount' => 56250,
         ],
+        [
+            'name' => 'Vitam',
+            'amount' => 21750,
+        ],
+        [
+            'name' => 'Compass',
+            'amount' => 70000,
+        ],
+        [
+            'name' => 'Atalian',
+            'amount' => 5500,
+        ],
+        [
+            'name' => 'Universiteit Leiden',
+            'amount' => 2500,
+        ],
+        [
+            'name' => 'Zorg',
+            'amount' => 30000,
+        ],
     ],
 ];
